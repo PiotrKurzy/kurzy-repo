49e3622a848e81397c8eb507ff9bf10e9a39a5c3
PiotrKurzy <pkurzydym@gmail.com>
2023-07-20 14:08:01 +0200

    update

--- NEXT/kurzy-next1/src/pages/index.tsx
+++ NEXT/kurzy-next1/src/pages/index.tsx
@@ -4,48 +4,52 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import { getSortedPostsData } from "../lib/posts";
 import Date from "../components/date";
+import { GetStaticProps } from "next";
 
-export async function getStaticProps() {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: { id: string; date: string; title: string }[];
+}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+        <link href="/dist/output.css" rel="stylesheet" />
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Jestem Kurzy. Lubię placki.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
-
-export default function Home({ allPostsData, date }) {
-  return (
-    <>
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>Jestem Kurzy. Lubię placki.</p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{" "}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
-        </section>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h2 className={utilStyles.headingLg}>Blog</h2>
-          <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title }) => (
-              <li className={utilStyles.listItem} key={id}>
-                <Link href={`/posts/${id}`}>{title}</Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
-                </small>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </Layout>
-    </>
-  );
-}
+};
 
 // import { useState } from "react";
 // import Link from "next/link";
